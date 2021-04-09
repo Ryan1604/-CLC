250e4d88e3924822f1e8d032aa082199d3daeb2a
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2021-04-09 19:49:05 +0700

    fixing

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dc47aa6f5cc0b199f963d983045356c4fad7932a5763146658b278998e798ca9
-size 415
+const {getDefaultConfig} = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
